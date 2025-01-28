24b20fb9bb561c6597c6f3885a2dc4870c80c856
Mansur Tiyes <tiyes@usc.edu>
2025-01-28 12:40:56 -0800

    all done with part 1

--- split.cpp
+++ split.cpp
@@ -11,13 +11,49 @@ the function below should be the only one in this file.
 */
 
 #include "split.h"
+#include <cstddef>
+using namespace std;
 
 /* Add a prototype for a helper function here if you need */
+void push_back(Node*& linkedlist, Node*& node);
 
+// 1) if head of the original ll is empty -> return
+// 2) else, set head of og ll as temporary
+// 3) move og head to point to next element
+// 4) check the value at temp either odd or even
+// 5) set temp->next to nulptr
+// 6) push_back to the corresponding list
+// 7) call recursive split again
 void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
-// WRITE YOUR CODE HERE
+  if (in==nullptr) {
+    return;
+  }
+  Node* temp = in;
+  in = in->next;
+  temp->next = nullptr;
+  if (temp->value%2==0){
+    push_back(evens, temp);
+  }
+  else {
+    push_back(odds, temp);
+  }
+  split(in, odds, evens);
 }
 
 /* If you needed a helper function, write it here */
+// push_back function, works like a normal push_back but using recursive method
+void push_back(Node*& linkedlist, Node*& node) {
+  if (linkedlist==nullptr){
+    linkedlist = node;
+    return;
+  }
+  if (linkedlist->next == nullptr){
+    linkedlist->next = node;
+    return;
+  }
+  else {
+    push_back(linkedlist->next, node);
+  }
+}
--- test_split.cpp
+++ test_split.cpp
@@ -10,8 +10,42 @@ g++ split.cpp test_split.cpp -o test_split
 */
 
 #include "split.h"
+using namespace std;
+#include <iostream>
 
 int main(int argc, char* argv[])
 {
-
+    // new ll created (delete at the end)
+    Node* ll = new Node(1, nullptr);
+   for (size_t i = 2; i < 11; i++)
+   {
+    Node* node1 = new Node(i, nullptr);
+    push_back(ll, node1);
+   }
+   Node* odds = nullptr;
+   Node* evens = nullptr;
+   split(ll, odds, evens);
+   delete ll;
+   Node* temp = odds;
+   while(temp!=nullptr){
+    cout << temp->value << endl;
+    temp = temp->next;
+   }
+   temp = evens;
+   while(temp!=nullptr){
+    cout << temp->value << endl;
+    temp = temp->next;
+   }
+   temp = odds;
+   while(temp!=nullptr){
+    Node* temp1 = temp;
+    temp = temp->next;
+    delete temp1;
+   }
+   temp = evens;
+   while(temp!=nullptr){
+    Node* temp2 = temp;
+    temp = temp->next;
+    delete temp2;
+   }
 }
